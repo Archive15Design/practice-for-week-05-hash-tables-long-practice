67fe59eea7a091dfbc70401d8496cb889b2ef3d1
Archive 15 Design <archive15design@gmail.com>
2023-10-25 13:27:13 +0200

    tests passed, no bonus work done

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -9,8 +9,9 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -30,22 +31,97 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
 
   insert(key, value) {
-    // Your code here
+    // check if new entry changes load factor
+    const loadFactor = this.count / this.capacity;
+    if (loadFactor > 0.7) this.resize();
+
+    let newPair = new KeyValuePair(key, value);
+    let hashKey = this.hashMod(key);
+    let current = this.data[hashKey];
+
+    // start iterating thru linked list, checking if key already exists
+    // if bucket is empty, loop will be skipped and if only one entry it will check if keys are the same
+    while (current) {
+      // if key exists, update value
+      if (current.key === newPair.key) {
+        current.value = newPair.value;
+        return;
+      }
+      current = current.next;
+    }
+
+    // if bucket is not empty, set correct next pointer for linked list
+    if (this.data[hashKey]) newPair.next = this.data[hashKey];
+
+    // finally insert new key value pair
+    this.data[hashKey] = newPair;
+    this.count++;
   }
 
 
+
   read(key) {
-    // Your code here
+    const hashKey = this.hashMod(key);
+    let current = this.data[hashKey];
+
+    // loop thru linked list until key is found
+    while (current){
+      if (current.key === key) return current.value;
+      current = current.next;
+    }
+
   }
 
 
   resize() {
-    // Your code here
+    this.capacity = this.capacity * 2;
+    let newTable = new Array(this.capacity).fill(null);
+    let oldTable = this.data;
+    this.data = newTable;
+    this.count = 0;
+
+    for (const bucket of oldTable){
+      let current = bucket;
+
+      while (current) {
+        this.insert(current.key, current.value);
+        current = current.next;
+      }
+
+    }
+    return 'resize complete';
   }
 
 
   delete(key) {
-    // Your code here
+    let hashKey = this.hashMod(key);
+
+    // check if bucket with entry exists, if not return
+    if (!this.data[hashKey]) return 'Key not found';
+
+    // delete key value pair when single entry or head of list
+    if (this.data[hashKey].key === key) {
+      this.data[hashKey] = this.data[hashKey].next;
+      this.count--;
+      return;
+    }
+
+    let current = this.data[hashKey];
+    let prev = current;
+    // start iterating using while loop
+    while (current) {
+      // check if current entry matches key
+      if (current.key === key) {
+        // if match, set previous entries pointer to currents next
+        prev.next = current.next;
+        this.count--;
+        return;
+      }
+      prev = current;
+      current = current.next;
+    }
+    // if key not found in list return with message
+    return 'Key not found';
   }
 }
 
--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,25 +1,60 @@
 function anagrams(str1, str2) {
-  // Your code here
+    let str1set = new Set(str1.split(''))
+    let str2split = str2.split('');
+
+    for (const char of str2split){
+      if(!str1set.has(char)) return false;
+    }
+
+    return true;
 }
 
 
 function commonElements(arr1, arr2) {
-  // Your code here
+  let commons = new Array();
+  const set1 = new Set(arr1);
+
+  arr2.forEach((element) => {
+    if (set1.has(element)) commons.push(element);
+  })
+  return commons;
 }
 
 
 function duplicate(arr) {
-  // Your code here
+  let set = new Set();
+  for (const ele of arr) {
+    if (!set.has(ele)) set.add(ele);
+    else return ele;
+  }
 }
 
 
 function twoSum(nums, target) {
-  // Your code here
+  let set = new Set(nums);
+  let difference = 0;
+
+  for (const num of nums) {
+    difference = target - num;
+    if (difference === num) continue;
+    else if (set.has(difference)) return true;
+  }
+  return false;
 }
 
 
 function wordPattern(pattern, strings) {
-  // Your code here
+  let hash = new Object();
+
+  for (let i = 0; i < pattern.length; i++) {
+
+    if (!hash[pattern[i]] && !hash[strings[i]]) {
+      hash[pattern[i]] = strings[i];
+      hash[strings[i]] = pattern[i];
+    } else if (hash[pattern[i]] !== strings[i]) return false;
+
+  }
+  return true;
 }
 
 
